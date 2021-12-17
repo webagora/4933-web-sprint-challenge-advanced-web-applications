8407c331e20ab074158a4ad6b376b06540080a37
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-17 09:36:11 -0800

    Logout Authentication - requirements fulfilled

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react"
+import { useHistory } from 'react-router-dom'
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Logout = () => {        
-    return(<div></div>);
+const Logout = () => {
+    const { push } = useHistory();
+    // 1. On mount, execute a http request to the logout endpoint.
+    useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    axiosWithAuth()
+        .post ('/logout')        
+        .then (resp => {            
+            // 2. On a successful request, remove the token from localStorage and redirect to the login page.          
+            localStorage.removeItem("token")
+            push ("/login");       
+        })
+
+        }, [])
+        return(<div></div>);
 }
 
 export default Logout;
 
 // Task List
-// 1. On mount, execute a http request to the logout endpoint.
-// 2. On a successful request, remove the token from localStorage and redirect to the login page.
+
