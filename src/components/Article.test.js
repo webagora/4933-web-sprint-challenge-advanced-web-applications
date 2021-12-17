cb0c60f7cbdd79e9dfc1db3716a8cc25df43c59a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-17 10:15:11 -0800

    Article.test.js - renders component without errors

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import userEvent from '@testing-library/user-event';
@@ -6,17 +7,36 @@ import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
+const testArticle = {
+    id: "",
+    author: "",
+    headline: "",
+    createdOn: "",
+    summary: "", 
+    body: "" 
+}
+
 test('renders component without errors', ()=> {
+    render (<Article article={
+        {
+            id: "",
+            author: "",
+            headline: "",
+            createdOn: "",
+            summary: "", 
+            body: "" 
+        } 
+    } />)
 });
 
-test('renders headline, author from the article when passed in through props', ()=> {
-});
+// test('renders headline, author from the article when passed in through props', ()=> {
+// });
 
-test('renders "Associated Press" when no author is given', ()=> {
-});
+// test('renders "Associated Press" when no author is given', ()=> {
+// });
 
-test('executes handleDelete when the delete button is pressed', ()=> {
-});
+// test('executes handleDelete when the delete button is pressed', ()=> {
+// });
 
 //Task List: 
 //1. Complete all above tests. Create test article data when needed.
