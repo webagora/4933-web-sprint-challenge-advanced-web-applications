39674e299fb2000cd2531764114cd98846bb0e60
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-17 06:39:43 -0800

    Route Authentication - requirements fulfilled

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Login from './Login';
 
 import Logout from './Logout';
 import View from './View';
+//1. Create and import PrivateRoute component.
+import PrivateRoute from './PrivateRoute';
 
 const App = () => {
   return (
@@ -15,11 +17,22 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
+
         {/* //3. Create a PrivateRoute for View component point to '/view.' */}
-        <Route exact path="/view" component={View} />
-        <Route path="/logout" component={Logout} />
+        <PrivateRoute exact path="/view" component={View} />
+        {/* <PrivateRoute exact path="/view" >
+          <View />
+        </PrivateRoute> */}
+
+        {/* //4. Create a PrivateRoute for Logout component pointing to '/logout.' */}
+        <PrivateRoute exact path="/logout" component={Logout} />
+        {/* <PrivateRoute path="/logout" >
+          <Logout />
+        </PrivateRoute> */}
+
         {/* //2. Create a Route for Login pointing to '/login.' */}
         <Route path="/login" component={Login} /> 
+
         <Route exact path="/">
           <Login/>
         </Route>          
@@ -31,9 +44,6 @@ const App = () => {
 export default App;
 
 //Task List
-//1. Create and import PrivateRoute component.
-//4. Create a PrivateRoute for Logout component pointing to '/logout.'
-
 
 const AppContainer = styled.div`
   height: 100%;
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,39 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-function PrivateRoute() {
-  return (<Route/>);
+//Task List:
+//1. Complete PrivateRoute
+
+/*
+export default function PrivateRoute (props) {
+  const {children, ...rest} = props;
+
+  return (<Route { ...rest } render = { () => {
+    if (localStorage.getItem("token")) {
+      return children;
+  } else {
+      return <Redirect to="/login"/>;
+  }
+  }} />)
+} 
+*/
+
+
+function PrivateRoute (props) {
+  const {component: Component, ...rest} = props;
+
+  // return (<Route { ...rest } render = { (props) => (localStorage.getItem("token")) ? <Component {...props}/> : <Redirect to="/login"/> } />);
+
+  return (<Route { ...rest } render = { (props) => {
+    if (localStorage.getItem("token")) {
+      return <Component {...props}/>
+  } else {
+      return <Redirect to="/login"/>
+  }
+  }} />);
 }
 
 export default PrivateRoute;
 
-//Task List:
-//1. Complete PrivateRoute
+
+
