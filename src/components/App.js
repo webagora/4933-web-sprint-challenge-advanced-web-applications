66774a658d99e89e853a93ef32a8a4701dd4f179
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-17 04:51:19 -0800

    Basic Routing - Build Routes rendering Login.js, View.js and Logout.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,20 @@ import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
 
+import Logout from './Logout';
+import View from './View';
+
 const App = () => {
   return (
     <AppContainer>
       <BloomHeader/>
       <Header/>
       <RouteContainer>
+        {/* //3. Create a PrivateRoute for View component point to '/view.' */}
+        <Route exact path="/view" component={View} />
+        <Route path="/logout" component={Logout} />
+        {/* //2. Create a Route for Login pointing to '/login.' */}
+        <Route path="/login" component={Login} /> 
         <Route exact path="/">
           <Login/>
         </Route>          
@@ -24,8 +32,6 @@ export default App;
 
 //Task List
 //1. Create and import PrivateRoute component.
-//2. Create a Route for Login pointing to '/login.'
-//3. Create a PrivateRoute for View component point to '/view.'
 //4. Create a PrivateRoute for Logout component pointing to '/logout.'
 
 
