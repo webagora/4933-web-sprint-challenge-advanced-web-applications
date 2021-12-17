d3c0d641f58d21ebefdc8d4aafde18a853835914
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-17 05:28:15 -0800

    Login Authentication - requirements fulfilled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Login = () => {
-    
+    //3. Add in necessary local state to support login form and error display.
+    const [login, setLogin] = useState({
+        username: '',
+        password: ''
+    })
+
+    const [errorMessage, setErrorMessage] = useState ("");
+
+    const { push } = useHistory();
+
+    const handleChange = (e) => {
+        setLogin({
+            ...login,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //4. When login form is submitted, make an http call to the login route. Save the auth token on a successful response and redirect to view page.
+    const handleSubmit = (e) => {
+        e.preventDefault();     
+        if (login.username === "" || login.password === "" ) {
+            //add in error action 
+            setErrorMessage("No empty of username, and password.");            
+            return;  
+        }   
+        axios.post('http://localhost:5000/api/login', login)  
+            .then(resp => {                
+                localStorage.setItem('token', resp.data.token);
+                push ('/view');
+            })
+            //5. If the response is not successful, display an error statement. **a server provided error message can be found in ```err.response.data```**
+            .catch(err => {
+                setErrorMessage(err.response.data.error);                
+                console.log('err: ', err);
+                return;
+            })
+    }
+
     return(<ComponentContainer>
         <ModalContainer>
             <h1>Welcome to Blogger Pro</h1>
             <h2>Please enter your account information.</h2>
+            {/* //1. Build login form DOM from scratch, making use of styled components if needed. Make sure the username input has id="username" and the password input as id="password". */}
+            <form 
+                onSubmit={handleSubmit}
+                >   
+                <UserInputs> 
+                <label> Username:
+                    <input 
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Enter your username"
+                        value={login.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                {/* <UserInputs> 
+                </UserInputs>  */}
+                <label> Password:
+                    <input 
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={login.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                </UserInputs>
+                <button id="submit">Log in</button>
+            </form>
+            {/* //2. Add in a p tag with the id="error" under the login form for use in error display. */}
+            { errorMessage && <p style={{ color: 'red' }} id="error">Error: {errorMessage} </p> }
         </ModalContainer>
     </ComponentContainer>);
 }
@@ -14,11 +85,6 @@ const Login = () => {
 export default Login;
 
 //Task List
-//1. Build login form DOM from scratch, making use of styled components if needed. Make sure the username input has id="username" and the password input as id="password".
-//2. Add in a p tag with the id="error" under the login form for use in error display.
-//3. Add in necessary local state to support login form and error display.
-//4. When login form is submitted, make an http call to the login route. Save the auth token on a successful response and redirect to view page.
-//5. If the response is not successful, display an error statement. **a server provided error message can be found in ```err.response.data```**
 //6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
 
 const ComponentContainer = styled.div`
@@ -32,6 +98,12 @@ const ModalContainer = styled.div`
     width: 500px;
     background: white;
     padding: 2rem;
+    text-align: center;
+`
+
+const UserInputs = styled.div`    
+    background: white;
+    padding: 1%;
     text-align: center;
 `
 
